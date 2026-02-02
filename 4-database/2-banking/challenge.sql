8eeeef0145f5b06017e1374e84981a1fa953f24f
ErickfuentesIT <erickfuentes.it@gmail.com>
2026-02-01 22:50:24 -0600

    refactor: applying suggested changes to exercise#2

--- 4-database/2-banking/challenge.sql
+++ 4-database/2-banking/challenge.sql
@@ -40,12 +40,13 @@
 */
 -- your solution here
 
-CREATE OR REPLACE FUNCTION transfer_funds(from_id INT, to_id INT, amount NUMERIC)
-RETURNS text as $body$
+CREATE OR REPLACE FUNCTION banking.transfer_funds(from_id INT, to_id INT, amount NUMERIC)
+RETURNS void as $body$
 DECLARE
 	ref_uuid UUID := gen_random_uuid(); -- GENERATING UUID
 	active_accounts INT;
 	sender_funds NUMERIC;
+	status_account INT;
 BEGIN
 -- VALIDATIONS WHERE CONSULTING DB IS NOT NEEDED
 	IF from_id = to_id THEN
@@ -64,28 +65,38 @@ SELECT
 	count(*) INTO active_accounts
 FROM accounts 
 WHERE account_id 
-IN (from_id, to_id) 
+IN (from_id, to_id);
+
+SELECT 
+	count(*) INTO status_account
+FROM accounts 
+WHERE account_id 
+IN (from_id, to_id)
 AND status != 'frozen';
 
 IF active_accounts = 2 THEN 
-	SELECT balance INTO sender_funds from accounts WHERE account_id = from_id;
-	IF sender_funds >= amount THEN
-		-- TRANSFERENCE EXECUTION
-		UPDATE accounts SET balance = balance - amount WHERE account_id = from_id;
-		UPDATE accounts SET balance = balance + amount WHERE account_id = to_id;
-		-- INSERTING LOGS INTO TRANSACTIONS TABLE LINKED BY UUID
-		INSERT INTO transactions (account_id, amount, transaction_type, reference, transaction_date)
-		VALUES (from_id, amount, 'withdrawal', ref_uuid, current_timestamp);
-
-		INSERT INTO transactions (account_id, amount, transaction_type, reference, transaction_date)
-		VALUES (to_id, amount, 'deposit', ref_uuid, current_timestamp);
-
-		RETURN 'Successful transfer. Reference: ' || ref_uuid;
-	ELSE
-		raise exception 'Insufficient funds. Available: %, Needed: %', sender_funds, amount;
+	IF status_account = 2 THEN 
+		SELECT balance INTO sender_funds from accounts WHERE account_id = from_id;
+		IF sender_funds >= amount THEN
+			-- TRANSFERENCE EXECUTION
+			UPDATE accounts SET balance = balance - amount WHERE account_id = from_id;
+			UPDATE accounts SET balance = balance + amount WHERE account_id = to_id;
+			-- INSERTING LOGS INTO TRANSACTIONS TABLE LINKED BY UUID
+			INSERT INTO transactions (account_id, amount, transaction_type, reference, transaction_date)
+			VALUES (from_id, amount, 'withdrawal', ref_uuid, current_timestamp);
+	
+			INSERT INTO transactions (account_id, amount, transaction_type, reference, transaction_date)
+			VALUES (to_id, amount, 'deposit', ref_uuid, current_timestamp);
+	
+			RAISE NOTICE 'Successful transfer. Reference: %', ref_uuid;
+		ELSE
+			RAISE EXCEPTION 'Insufficient funds. Available: %, Needed: %', sender_funds, amount;
+		END IF;
+	ELSE 
+		RAISE EXCEPTION 'Account status failed - From: (ID) % and/or To: (ID) % accounts have a frozen status', from_id, to_id;
 	END IF;
 ELSE
-	raise exception 'Accounts either do not exist or are frozen';
+	RAISE EXCEPTION 'Account validation failed - From: (ID) % and/or To: (ID) % accounts not found in database.', from_id, to_id;
 END IF;
 
 END $body$
